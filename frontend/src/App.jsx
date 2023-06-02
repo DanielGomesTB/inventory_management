c774edb1c403fe61b811489747259a9016a3819f
Danilo Ribeiro <nilo22@gmail.com>
2023-06-02 17:43:41 -0300

    refactor(frontend): remove unecessary files and create Context files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import './App.css'
 import { getAllProducts } from './service/api'
 
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import Provider from './context/Provider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
