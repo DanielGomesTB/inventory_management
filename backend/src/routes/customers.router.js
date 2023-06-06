4335ee6d9d1ad2476d21484ce67d7099f5eb90e1
Daniel Gomes <danielgomesveiga@hotmail.com>
2023-06-06 17:53:22 -0300

    feat(backend): create update route for customers table

--- backend/src/routes/customers.router.js
+++ backend/src/routes/customers.router.js
@@ -5,5 +5,6 @@ const customersRouter = express.Router();
 
 customersRouter.get('/customers', customersController.getAll)
 customersRouter.post('/customers', customersController.insert)
+customersRouter.put('/customers/:id', customersController.update)
 
 module.exports = customersRouter;
