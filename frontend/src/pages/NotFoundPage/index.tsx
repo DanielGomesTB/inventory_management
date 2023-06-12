311753a8a9119fc1f7cb9d316dc8b5c6282a021a
marcelo marques <marcelo-mls@hotmail.com>
2023-06-12 16:06:24 -0300

    refactor(frontend): change the message on the not found page

--- frontend/src/pages/NotFoundPage/index.tsx
+++ frontend/src/pages/NotFoundPage/index.tsx
@@ -2,7 +2,7 @@ export default function NotFoundPage() {
   return (
     <div>
       <h1>Ops! ¯\_(ツ)_/¯</h1>
-      <h2>Ops! Parece que essa caixa está vazia. Talvez ela tenha sido movida para um local desconhecido. Vamos ajudá-lo a encontrar o caminho certo!</h2>
+      <h2>Parece que essa caixa está vazia. Talvez ela tenha sido movida para um local desconhecido. Vamos ajudá-lo a encontrar o caminho certo!</h2>
       <br />
       <p>404 - Page Not Found</p>
       <p>The requested page does not exist.</p>
